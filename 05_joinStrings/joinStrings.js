f18ea365b03ba6d6dd72aadac79335d21fbf6d64
stebeus <stebeus@mailfence.com>
2025-10-04 15:47:32 -0300

    Replace previous constants with fullName and age in greeting constant.

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -17,9 +17,7 @@ const birthYear = 1947;
 const thisYear = 1965;
 const age = thisYear - birthYear;
 
-const greeting = `Hello! My name is ${firstName} ${lastName} and I am ${
-  thisYear - birthYear
-} years old.`;
+const greeting = `Hello! My name is ${fullName} and I am ${age} years old.`;
 
 // Do not change this
 module.exports = {
