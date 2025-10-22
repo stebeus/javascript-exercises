17623b4f12b2c4a40126c02acc24aa0b43b11879
stebeus <stebeus@mailfence.com>
2025-10-22 14:29:11 -0300

    Create string reverse function

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  return str.split('').reverse().join('');
 };
+
+console.log(reverseString('foo 123!'));
 
 // Do not edit below this line
 module.exports = reverseString;
