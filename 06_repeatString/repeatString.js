b806177bfb4e19a1b74a245647f3e0ed9ce6b880
stebeus <stebeus@mailfence.com>
2025-10-22 14:21:31 -0300

    Create repeatString function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+  let strCollection = [];
 
+  if (num < 0) {
+    return 'ERROR';
+  } else {
+    for (i = 0; i < num; i++) {
+      strCollection.push(str);
+    }
+
+    return strCollection.join('');
+  }
 };
+
+console.log(repeatString('foo', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
