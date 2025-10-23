78fc5e5d7f1c6e545f9dc237ea83ba65ac73019c
stebeus <stebeus@mailfence.com>
2025-10-23 13:46:37 -0300

    Create sumAll function

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+  let numCollection = [];
 
+  if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) {
+    return 'ERROR';
+  } else if (numOne < 0 || numTwo < 0) {
+    return 'ERROR';
+  } else if (numOne === '' && numTwo === '') {
+    return 'ERROR';
+  } else if (numOne > numTwo) {
+    for (let i = numOne; i >= numTwo; i--) {
+      numCollection.push(i);
+    }
+  } else {
+    for (let i = numOne; i <= numTwo; i++) {
+      numCollection.push(i);
+    }
+  }
+
+  return numCollection.reduce((totalSum, currentNum) => totalSum + currentNum);
+
+  // return numCollection.reduce((a, b) => a + b);
 };
+
+console.log(sumAll(2.5, 2));
 
 // Do not edit below this line
 module.exports = sumAll;
