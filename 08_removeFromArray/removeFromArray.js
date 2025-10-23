f7041a9b00b929c8797c0202c4eae5c0544d0159
stebeus <stebeus@mailfence.com>
2025-10-23 12:47:23 -0300

    Create reemoveFromArray function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...indexes) {
+  const indexOf = arr.indexOf(indexes);
+
+  if (indexOf > -1) {
+    arr.splice(indexOf, 1);
+  }
+
+  return arr.filter((x) => !indexes.includes(x));
 };
+
+console.log(removeFromArray([1, 2, 3, 4, 5]));
 
 // Do not edit below this line
 module.exports = removeFromArray;
