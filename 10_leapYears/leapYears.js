0f5b0e73739e1a80d3eca089dbdea5550ae30cfa
stebeus <stebeus@mailfence.com>
2025-10-25 07:21:15 -0300

    Create leapYears function

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0 && year % 400 !== 0) {
+      return false;
+    }
+    return true;
+  } else {
+    return false;
+  }
 };
+
+console.log(leapYears(2004));
+console.log(leapYears(2400));
+console.log(leapYears(2000));
+
+console.log(leapYears(1945));
+console.log(leapYears(1900));
 
 // Do not edit below this line
 module.exports = leapYears;
