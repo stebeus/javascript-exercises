b5744da05e83740cc73ae140421a40f588d3a1f3
stebeus <stebeus@mailfence.com>
2025-10-26 07:08:48 -0300

    Create celsius converter function

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  const celsius = ((temperature - 32) * 5) / 9;
+  const roundedTemperature = Math.round(celsius * 10) / 10;
+  return roundedTemperature;
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function () {};
+
+console.log(convertToCelsius(57));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
