d9cc8e1e8d63c53cf6df934e55e5089ed585426e
stebeus <stebeus@mailfence.com>
2025-11-07 14:10:43 -0300

    Make sum() functional

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -6,7 +6,15 @@ const subtract = function (numOne, numTwo) {
   return numOne - numTwo;
 };
 
-const sum = function () {};
+const sum = function (numArr) {
+  if (numArr.length === 0) {
+    return 0;
+  }
+
+  return numArr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
+};
 
 const multiply = function (numOne, numTwo) {
   return numOne * numTwo;
@@ -18,8 +26,10 @@ const power = function (numOne, numTwo) {
 
 const factorial = function () {};
 
-console.log(add(1, 5, 10));
+console.log(add(1, 5));
 console.log(subtract(19, 5));
+console.log(sum([]));
+console.log(sum([1, 2, 3, 10]));
 console.log(multiply(2, 5));
 
 // Do not edit below this line
