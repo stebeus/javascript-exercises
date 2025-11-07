83c76df8175d10d8572af8e0230354251cde337e
stebeus <stebeus@mailfence.com>
2025-11-07 16:47:01 -0300

    Write algorithm for palindromes function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {};
 
-};
+/**
+ * Get the string
+ * Lower case it
+ * Filter non-alphanumeric characters
+ * Evaluate order by:
+ * Creating inverted order
+ * Compare it with original order
+ * If both are equal, return true
+ * Else, return false.
+ */
 
 // Do not edit below this line
 module.exports = palindromes;
