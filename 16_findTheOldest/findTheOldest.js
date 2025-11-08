95b2f08f117300ce244f9210cfa969ea13141bd2
stebeus <stebeus@mailfence.com>
2025-11-08 11:21:32 -0300

    Create findTheOldest algorithm

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,33 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  /**
+   * Get the people
+   * Create an empty age list array
+   * For each person of people:
+   *   Calculate death year minus birth year
+   *   Push result to age list
+   * Return maximum age
+   */
 };
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
