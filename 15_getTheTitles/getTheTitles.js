60879de5ed39997683e4ba093ee6e69a87e84341
stebeus <stebeus@mailfence.com>
2025-11-08 08:49:08 -0300

    Add map method in getTheTitles function

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  /**
+   * Get the books
+   * Map books
+   * Get each title in book
+   * Return it
+   */
 
+  return books.map((book) => book.title);
 };
 
 // Do not edit below this line
