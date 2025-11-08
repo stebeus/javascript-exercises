155cd242bb20c280b02b596624c361728e4d3527
stebeus <stebeus@mailfence.com>
2025-11-08 07:22:55 -0300

    Create array fibonnaciSequence variable

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -6,7 +6,13 @@ const fibonacci = function (fibonacciIndex) {
    * Push it to the array
    * Return the number of that index
    */
+
+  const fibonacciSequence = [];
+
+  return fibonacciSequence[fibonacciIndex];
 };
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
