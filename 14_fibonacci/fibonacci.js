0e9f265f2c44989ffb1beac9af590c9fa228a2a0
stebeus <stebeus@mailfence.com>
2025-11-08 07:14:26 -0300

    Create fibonnaci algorithm

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (fibonacciIndex) {
+  /**
+   * Get the index
+   * Create an array
+   * Loop the sequence up to the index
+   * Push it to the array
+   * Return the number of that index
+   */
 };
 
 // Do not edit below this line
