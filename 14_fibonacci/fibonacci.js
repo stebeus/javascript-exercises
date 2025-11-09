ba65219aa05c55ecfbadcc98fba9426a0d51cba2
stebeus <stebeus@mailfence.com>
2025-11-09 08:13:08 -0300

    Return error for negative numbers

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -2,11 +2,16 @@ const fibonacci = function (fibonacciIndex) {
   /**
    * Get the index
    * Store an empty Fibonacci sequence array
+   * Checks if number is negative
    * Push each looped item to the array
    * Return the item indicated by index
    */
 
   const fibonacciSequence = [];
+
+  if (fibonacciIndex < 0) {
+    return "OOPS";
+  }
 
   for (let num = 0; num <= fibonacciIndex; num++) {
     fibonacciSequence.push(num);
