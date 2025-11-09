b4155e23e98cedebb5606e0d5caf7aabda326046
stebeus <stebeus@mailfence.com>
2025-11-09 08:51:46 -0300

    Clear code to use new algorithm

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,32 +1,4 @@
-const fibonacci = function (numIndex) {
-  /**
-   * Get the index
-   * Store an empty Fibonacci sequence array
-   * Checks if number is negative
-   * Return index number if it is less or equal to one
-   * Push each looped item to the array
-   * Return the item indicated by index
-   */
-
-  const fibonacciSequence = [];
-
-  if (numIndex < 0) {
-    return "OOPS";
-  } else if (numIndex <= 1) {
-    return numIndex;
-  }
-
-  for (let num = 0; num <= numIndex; num++) {
-    fibonacciSequence.push(num);
-  }
-
-  return fibonacciSequence[numIndex];
-};
-
-console.log(fibonacci(12));
-console.log(fibonacci("6"));
-console.log(fibonacci("0"));
-console.log(fibonacci("-15"));
+const fibonacci = function () {};
 
 // Do not edit below this line
 module.exports = fibonacci;
