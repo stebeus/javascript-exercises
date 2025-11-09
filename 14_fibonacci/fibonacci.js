a6dfdcaeb1991fd62ef557e6934841cf65b17d92
stebeus <stebeus@mailfence.com>
2025-11-09 07:46:28 -0300

    Return index item in dummy Fibonacci algorithm

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -3,6 +3,10 @@ const fibonacci = function (fibonacciIndex) {
    * Get the index
    * Return the item indicated by index
    */
+
+  const fibonacciSequence = [0, 1, 1, 2, 3, 5, 8, 13, 21];
+
+  return fibonacciSequence[fibonacciIndex];
 };
 
 console.log(fibonacci(6));
