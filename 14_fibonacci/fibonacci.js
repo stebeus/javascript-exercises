5b75395069b0e80c8eab5b33314aecc05ecf7691
stebeus <stebeus@mailfence.com>
2025-11-09 08:53:57 -0300

    Create console log test units

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
 const fibonacci = function () {};
 
+console.log(fibonacci(10));
+console.log(fibonacci(6));
+console.log(fibonacci(3));
+console.log(fibonacci(1));
+console.log(fibonacci(0));
+console.log(fibonacci(-10));
+console.log(fibonacci("3"));
+console.log(fibonacci("1"));
+console.log(fibonacci("0"));
+console.log(fibonacci("-6"));
+
 // Do not edit below this line
 module.exports = fibonacci;
