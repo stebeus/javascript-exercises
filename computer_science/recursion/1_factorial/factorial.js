61190f690f1e81b52d79f82c301218f0f4f41274
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-24 10:43:20 -0300

    Create factorial recursive function

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,9 @@
-const factorial = function() {
+const factorial = function (number) {
+  if (number === 0 || number === 1) return number;
 
+  if (!Number.isInteger(number)) return undefined;
+
+  return number * factorial(number - 1);
 };
 
 // Do not edit below this line
